39ce736ce767aa3b7246ed4a273eb4bf3c70e92a
Nilupul Perera <nilupulperera@gmail.com>
2022-10-16 19:44:04 +0530

    Add Previous and Next buttons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+// Collapsible menu icon for mobile version
+
 const collapsibles = document.querySelectorAll('.collapsible');
 collapsibles.forEach((item) =>
 	item.addEventListener('click', function () {
 		this.classList.toggle('collapsible--expanded');
 	})
 );
+
+// #############################################
+
+// When the user scrolls down 50px from the top of the document, resize the back-button's opacity
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
+    document.getElementById("back-button").style.display = "inline";
+  } else {
+    document.getElementById("back-button").style.display = "none";
+  }
+}
