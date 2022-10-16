1ead3a1314a312b36b9fe1c8d129a603856c6b40
Nilupul Perera <nilupulperera@gmail.com>
2022-10-16 22:01:33 +0530

    Back button to #back-heading

--- js/main.js
+++ js/main.js
@@ -16,7 +16,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
     document.getElementById("back-button").style.display = "inline";
+    document.getElementById("back-button").style.opacity = "0.8";
   } else {
     document.getElementById("back-button").style.display = "none";
+    document.getElementById("back-button").style.opacity = "0";
   }
 }
